2221ce35e9389b2e99b109c782865715c139273f
adrianqm <adrianqm97@gmail.com>
2024-01-26 18:03:24 +0100

    Adding a lot of asset database

--- Runtime/Scriptable Objects/Graph/PortSO.cs
+++ Runtime/Scriptable Objects/Graph/PortSO.cs
@@ -30,6 +30,7 @@ namespace AQM.Tools
         public void SaveAs(DialogSystemDatabase db)
         {
             AssetDatabase.AddObjectToAsset(this,db);
+            AssetDatabase.SaveAssets();
         }
 
         public void SortPort()
